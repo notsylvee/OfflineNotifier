b768370fe3a98cb204f15928167772afad3b1599
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-04-07 00:06:00 -0400

    updated offline message and added all new bots

--- src/events/offlineNotifier.js
+++ src/events/offlineNotifier.js
@@ -5,17 +5,24 @@ module.exports = {
     async execute (oldStatus, newStatus, client) {
         const channel = await client.channels.fetch("1344570100589662268");
         const botIDs = [
-            "1337978432700682371", /*CT*/
-            "1337995599743815734", /*E8B*/
-            "1318490033464021032", /*IF*/
-            "1334767311537836133", /*RT*/
-            "1338314970303037440", /*SS*/
-            "953885359668871238", /*SB*/
-            "1338726967142256716", /*TB*/
+            "1337978432700682371", /*Coolio Toolio*/
+            "1353297715165007873", /*Dwayne The Rock Johnson*/
+            "1357541155973107956", /*El Goblino*/
+            "1337995599743815734", /*Enchanted 8 Ball*/
+            "1358328263998439445", /*Gabriel*/
+            "1318490033464021032", /*Imaginary Friend*/
+            "1353099114203840532", /*OmniBot*/
+            "1355611589054824469", /*Quag*/
+            "1334767311537836133", /*Red Teddy*/
+            "1338314970303037440", /*Sebastian Solace*/
+            "1353297778163322973", /*Steve*/
+            "953885359668871238", /*Sylvbot*/
+            "1338726967142256716", /*Teddy Bloxpin*/
         ];
         if (!botIDs.includes(newStatus.user.id)) return;
         if (oldStatus.status !== "offline" && newStatus.status == "offline") {
-            channel.send(`<@${oldStatus.user.id}> has gone offline. <@1018686464000807003> please fix`)
+            channel.send(`<@&1358635798991208558>, <@${oldStatus.user.id}> has gone offline.
+                \n<@1018686464000807003> will fix this when she can`)
         };
     },
 };
